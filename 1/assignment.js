51192e7681d7d834fb2f98fc0d64f596f17cf34f
nastya <ashev1996@mail.ru>
2020-10-14 12:59:35 +0300

    Homework 1

---
+++ 1/assignment.js
@@ -0,0 +1,9 @@
+let result, userNumber = 5;
+
+result = 18 + userNumber;
+result = result - 10;
+result = result * 4;
+result = result / 2;
+
+alert('число пользователя = ' + userNumber);
+alert('результат = ' + result);
